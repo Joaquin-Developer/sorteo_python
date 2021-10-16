8ece68932211a2fe6f786023a21c8c2777c68de2
Joaquin-Parrilla <jparrilla@scanntech.com>
2021-10-16 15:47:28 -0300

    sorteo funcional

---
+++ main.py
@@ -0,0 +1,31 @@
+from os import replace
+import utils as utils
+
+def run_the_draw():
+    data = utils.get_data()
+    groups_draw = [[] for x in data[0]]
+    # Groups:
+    for i in range(0, len(groups_draw)):
+        # Data:
+        for j in range(0, len(data)):
+            index = utils.get_random_index_from_list(max=len(data[j]) - 1)
+            groups_draw[i].append(data[j][index])
+            # remove raffled team in bombo:
+            data[j].pop(index)
+    
+    return groups_draw
+
+def print_the_draw(groups_draw):
+    groups_letters = list(map(chr, range(65, 65 + len(groups_draw))))
+
+    print("========================================")
+    for i in range(0, len(groups_letters)):
+        print(f"GRUPO {groups_letters[i]}")
+        for group in groups_draw[i]:
+            print(f"- {group}")
+        print("========================================")
+
+
+if __name__ == "__main__":
+    groups_draw = run_the_draw()
+    print_the_draw(groups_draw)
---
+++ utils.py
@@ -0,0 +1,33 @@
+import os, datetime
+import random
+
+DATA_PATH = "data/"
+
+def read_csv():
+    path = f"{DATA_PATH}teams.csv"
+    data = []
+    with open(path) as file:
+        lines = file.readlines()
+
+    for line in lines:
+        data.append(line.replace("\n", ""))
+    
+    return data
+
+def get_data(in_json=True):
+    data = read_csv()
+    teams_data = []
+
+    for elem in (data):
+        teams_data.append(elem.split(":"))
+    return teams_data
+
+
+def to_csv(data):
+    dt = datetime.datetime.now()
+    path = f"{DATA_PATH}SORTEO {dt.day}-{dt.month}-{dt.year} {dt.hour}:{dt.minute}.csv"
+    with open(path, "w") as file:
+        file.write(data)
+
+def get_random_index_from_list(max):
+    return random.randint(0, max)
