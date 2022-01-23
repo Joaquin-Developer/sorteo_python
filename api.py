b438f6e1cef561b9799571018094a524e6af9c62
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-23 00:32:29 -0300

    refactor

--- api.py
+++ api.py
@@ -1,11 +1,20 @@
-from flask import Flask, json
+from flask import Flask
 from main import Draw
 
 app = Flask(__name__)
 
-@app.get("/api/v1/get_last_sorteo")
-def get_last_sorteo():
-    # return Draw.get_last_draw()
+
+@app.get("/")
+def index():
+    return """
+        <h1>Index</h1><hr />
+        <p>Visit: <a href="/api/v1/get_last_draw">/api/v1/get_last_draw</a>
+        for get last draw in json format. </p>
+    """
+
+
+@app.get("/api/v1/get_last_draw")
+def get_last_draw():
     return Draw.main()
 
 
