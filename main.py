79f3b177f8bfad9f484486211b670c295cad13c4
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-01-22 22:35:03 -0300

    FIX -> get_name_of_last_draw

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import os
 import subprocess
-import datetime
 from datetime import datetime
 import utils as utils
 
 
 class Draw():
+    """draw class"""
 
     @staticmethod
     def run_the_draw():
@@ -32,20 +31,7 @@ class Draw():
             print(f"GRUPO {groups_letters[i]}")
             for group in groups_draw[i]:
                 print(f"- {group}")
-            print("========================================")
-
-
-    @staticmethod
-    def main():
-        draw = Draw.get_last_draw()
-        if draw is not None: 
-            return draw
-
-        # si no hay sorteos hacemos uno nuevo, y luego lo retornamos:
-        groups_draw = Draw.run_the_draw()
-        utils.to_csv(groups_draw)
-        return utils.draw_to_json(groups_draw)
-        
+            print("========================================")        
 
     @staticmethod
     def get_last_draw():
@@ -58,18 +44,15 @@ class Draw():
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             
             lines = str(command.stdout, "UTF-8").split("\n")
-            # eliminamos las primeras 3 lineas  y las últimas 2:
-            del lines[0:3]
-            del lines[-1]
+            filenames = list(filter(lambda x: "SORTEO" in x, lines))
             
-            # si solo queda un archivo (teams.csv) o ninguno, es porque no hay sorteos.
-            if len(lines) < 2: 
+            # si no hay archivos, es porque no hay sorteos.
+            if len(filenames) == 0:
                 return None
 
-            # el primer elemento es el ultimo sorteo: 
-            last_draw = lines[0]
-            print(last_draw)
-            args = last_draw.split(" ")
+            # el primer elemento es el ultimo sorteo:
+            name_last_draw = filenames[0]
+            args = name_last_draw.split(" ")
             # created_at = " ".join(args[10:]).replace(".csv", "")
             # created_at = datetime.strptime(created_at, "%d-%m-%Y %H:%M")
             return " ".join(args[9:])
@@ -79,10 +62,20 @@ class Draw():
             return None
 
         path = utils.DATA_PATH + filename
-        # lo que sigue es obteber el archivo desde su nombre...
+        return utils.draw_to_json(utils.read_csv(path))
 
-        print(get_name_of_last_draw())
+    @staticmethod
+    def main():
+        """Return json of Draw"""
 
+        draw = Draw.get_last_draw()
+        if draw is not None:
+            return draw
+
+        # si no hay sorteos, generamos uno:
+        groups_draw = Draw.run_the_draw()
+        utils.to_csv(groups_draw)
+        return utils.draw_to_json(groups_draw)
 
 
 if __name__ == "__main__":
